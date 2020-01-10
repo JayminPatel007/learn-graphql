548194fed3aeb1f3a0c7064e1d2d21ff885bf181
Jaymin Patel <jaymin.future365@gmail.com>
2020-01-10 11:59:50 +0530

    Basic GraphQL

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("Hello world;");
+import {GraphQLServer} from 'graphql-yoga'
+
+const typeDefs = `
+    type Query{
+        hello: String!
+    }
+`
+
+const resolvers = {
+    Query: {
+        hello(){
+            return "This is my first string!"
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+});
+
+server.start(()=>{
+    console.log("Server is up")
+})
