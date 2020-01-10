ae5e95c6b908c32e73b49015a1b232f96adef368
Jaymin Patel <jaymin.future365@gmail.com>
2020-01-10 14:29:40 +0530

    Added Relation b/w User and Post

--- src/index.js
+++ src/index.js
@@ -1,10 +1,56 @@
 import {GraphQLServer} from 'graphql-yoga'
 
+const users = [
+    {
+        id: '1',
+        name: "jaymin Patel",
+        email: "jaymin.future365@gmail.com",
+        age: 22
+    },
+    {
+        id: '2',
+        name: "Arzoo",
+        email: "arzoo@gmail.com"
+    },
+    {
+        id: '3',
+        name: "Brijesh",
+        email: "brijesh@gmail.com",
+        age: 20
+    }
+]
+
+const posts = [
+    {
+        id: '1',
+        title: "CS",
+        body: "CS is awsome",
+        published: true,
+        author: '1'
+    },
+    {
+        id: '1',
+        title: "MBBS",
+        body: "MBBS is great",
+        published: true,
+        author: '2'
+    },
+    {
+        id: '1',
+        title: "Pharmacy",
+        body: "Pharmasy is OK",
+        published: false,
+        author: '3'
+    }
+]
+
 const typeDefs = `
     type Query{
+        users: [User!]
         greeting(name: String): String!
         me: User!
         post: Post!
+        posts: [Post!]!
     }
     type User{
         id: ID!
@@ -17,11 +63,18 @@ const typeDefs = `
         title: String!
         body: String!
         published: Boolean!
+        author: User!
     }
 `
 
 const resolvers = {
     Query: {
+        users(){
+            return users
+        },
+        posts(parent, args, ctx, info){
+            return posts
+        },
         greeting(parent, args, ctx, info){
             return `Hi, ${args.name}`
         },
@@ -41,6 +94,13 @@ const resolvers = {
                 "body": "GrapgQL is awsome!",
                 "published": true
             }
+        }
+    },
+    Post: {
+        author(parent, args, ctx, info){
+            return users.find(user=>{
+                return user.id === parent.author
+            })
         }
     }
         
